2b961cf75e8d2abb1534036cbd1dd13353e05c10
Francis Serpa <fcserpa@gmail.com>
2015-09-10 12:20:16 -0700

    ready for heroku deploy attempt #4

--- models/event.js
+++ models/event.js
@@ -18,7 +18,7 @@ var EventSchema = new Schema({
 var SlotsSchema = new Schema({
 	name: {type: String, required: true},
 	email: {type: String, required: true},
-	piece {type: String, required: true},
+	piece: {type: String, required: true},
 })
 
 //create a new event
